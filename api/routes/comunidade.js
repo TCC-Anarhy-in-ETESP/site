22deff6a56acd08dca727877567c2713bdfb7dd4
NicolasGaioviczAvila <nicolas.gaiovicz.avila@gmail.com>
2024-08-16 23:16:01 -0300

    comunidade api pronta

--- api/routes/comunidade.js
+++ api/routes/comunidade.js
@@ -40,13 +40,32 @@ router.post("/post-comment", auth, async (req, res) => {
     }
 });
 
-router.get("/get-posts", async (req, res) => {
+router.get("/get-posts-with-comments", async (req, res) => {
     try{
-        const resultado = await db.query("select * from getposts()")
+        const res_posts = await db.query("select * from getposts()");
+
+        var datas = []; 
+
+        const length = res_posts.length;
+        for(i = 0; i < length; i++){
+            const {p_id_post} = res_posts[i];
+            const res_comments = await db.query("select * from getcommentsbypost($1)", p_id_post);
+           
+            const data = {
+                post_comments : {
+                    post: await res_posts[i],
+                    comments: await res_comments
+                }
+            }
+
+            datas.push(data)
+        }
+        
         res.status(200).json({
             mensagem: "Todos os posts pegos com sucesso.",
-            resultados: await resultado
+            resultados: datas
         });
+        
     }catch(err){
         res.status(404).json({
             mensagem: "Erro, talvez o banco de dados esteja fora do ar.",
